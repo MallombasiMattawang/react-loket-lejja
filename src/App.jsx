705600b12b7b84c1155e660200e1170bb0a39c70
MallombasiMattawang <mattawang.rpl@gmail.com>
2024-03-06 09:53:22 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import Toaster
+import { Toaster } from 'react-hot-toast';
+
+//import routes
+import Routes from './routes';
+
+function App() {
+
+  return (
+    <>
+      <Routes />
+      <Toaster />
+    </>
+  )
+}
+
+export default App
